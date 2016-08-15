65be50738bfdb4529e17d7c4c65b36e1d6d45eec
Renaud <renaud.valkenberg@gmail.com>
2016-08-15 15:27:46 +0200

    Create Owned.sol

---
+++ Token_Contracts/contracts/Owned.sol
@@ -0,0 +1,20 @@
+/*
+Owned Contract used to restrict access to functionalities such as Mint Token and Remove Token
+*/
+
+contract Owned {
+    address public owner;
+
+    function Owned() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        if (msg.sender != owner) throw;
+        _
+    }
+
+    function transferOwnership(address newOwner) onlyOwner {
+        owner = newOwner;
+    }
+}
